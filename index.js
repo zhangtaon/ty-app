3771e7df669e5325aa8c220fe1065a7836f058b0
张涛 <zhangtao@tsingyun.net>
2019-10-24 00:10:37 +0800

    登录路由

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import store from "./lib/store"
 import getAppRoute from "./lib/util"
 import App from "./lib/App.vue"
+import { routes } from "./lib/route"
 
-export { getAppRoute, App, store}
+export { getAppRoute, App, store, routes }
