7886311fa7dba80ffbce4c13a142de6258c8dd90
张涛 <zhangtao@tsingyun.net>
2019-10-25 01:26:33 +0800

    项目名称应用package文件的description字段

--- lib/App.vue
+++ lib/App.vue
@@ -1,15 +1,15 @@
 <template>
   <el-container class="app">
     <el-header height="72px">
-      <div class="box">
-        <div class="media">
-          <div class="media-left">
-            <img src="./image/logo-header.png" alt />
-          </div>
-          <div class="media-body media-middle">TY智能化工管理系统</div>
+      <div class="box-between-center">
+        <div class="box-between-center logo">
+          <img src="./image/logo-header.png" alt />
+          <span>{{systemName}}</span>
         </div>
         <div class="operate">
-          <router-link to="/gis"><img src="./image/home.png" alt /></router-link>
+          <router-link to="/gis">
+            <img src="./image/home.png" alt />
+          </router-link>
           <a>
             <img src="./image/menu.png" alt />
           </a>
@@ -47,6 +47,11 @@ export default {
       menus: state => state.menu.menus
     })
   },
+  data() {
+    return {
+      systemName: require("../../../package.json").description
+    };
+  },
   components: {
     TyMenuTree
   },
@@ -61,39 +66,35 @@ body,
 html {
   height: 100%;
 }
+// 两边垂直居中
+.box-between-center {
+  display: -webkit-flex; /* Safari */
+  display: flex;
+  justify-content: space-between; //两侧的间隔相等
+  align-items: center; //垂直居中
+  flex-direction: row; //从左到右
+  height: 100%;
+}
+// 左右垂直居中
+.box-center {
+  display: -webkit-flex; /* Safari */
+  display: flex;
+  justify-content: center; //两侧的间隔相等
+  align-items: center; //垂直居中
+  flex-direction: row; //从左到右
+  height: 100%;
+}
 .app {
-  .el-menu-item,
-  .el-submenu {
-    text-align: left;
-  }
-  .el-submenu__title,
-  .el-menu-item{
-    font-size: 20px;
-    background-color: #F6F6F6
-  }
-
-  .el-row {
-    // line-height: 40px;
-    font-size: 14px;
-  }
   .el-header {
     background-color: #273342;
     height: 72px;
-    .box {
-      display: -webkit-flex; /* Safari */
-      display: flex;
-      justify-content: space-between; //两侧的间隔相等
-      align-items: center; //垂直居中
-      flex-direction: row; //从左到右
-      height: 100%;
-    }
-    .media {
+    .logo {
       font-family: AlibabaPuHuiTi-Medium;
       font-size: 24px;
       color: #ffffff;
       letter-spacing: 0;
       line-height: 34px;
-      width: 400px;
+      // width: 400px;
       img {
         width: 34px;
       }
@@ -101,8 +102,6 @@ html {
     .operate {
       a {
         width: 65px;
-        height: 72px;
-        line-height: 72px;
         display: inline-block;
         text-align: center;
         cursor: pointer;
@@ -110,23 +109,24 @@ html {
     }
   }
   .el-aside {
-    background-color: #F6F6F6;;
+    background-color: #f6f6f6;
     color: #333;
     text-align: center;
     line-height: 200px;
+    .el-menu-item,
+    .el-submenu {
+      text-align: left;
+    }
+    .el-submenu__title,
+    .el-menu-item {
+      font-size: 20px;
+      background-color: #f6f6f6;
+    }
   }
   .el-main {
     background-color: #e9eef3;
     color: #333;
-    // text-align: center;
-    // line-height: 160px;
   }
-  // body > .el-container {
-  margin-bottom: 40px;
   height: 100%;
-  // }
-  .el-icon-delete {
-    margin-left: 10px;
-  }
 }
 </style>
